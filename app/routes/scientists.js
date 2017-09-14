acf10e5c7d651d695e69e9a0bb9590acfa13c1af
Sarah Ricker <sricker@envision-creative.com>
2017-09-13 22:24:07 -0500

    Creates people-list ui component - replaces scientists with reusable component

--- app/routes/scientists.js
+++ app/routes/scientists.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return ['David Hanson, Sarah Ricker, Giovanni Penis Tree, Fish 1, Fish 2, Monster']
+    return ['David Hanson', 'Sarah Ricker', 'Giovanni Penis Tree', 'Fish 1', 'Fish 2', 'Monster']
   }
 });
